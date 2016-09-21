82eabe6d1ce112a6eeca2f600ddd2b6963cc8987
Anni Piragauta <aapiragautau@unal.edu.co>
2016-09-21 15:47:36 -0500

    Prueba todos los casos

---
+++ Soluciones/casesTest.py
@@ -0,0 +1,49 @@
+import filecmp
+x = 1
+
+print ">>> Casos L1A"
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1A_2016_1.out', 'salidaL1A_1.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1A_2016_2.out', 'salidaL1A_2.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1A_2016_3.out', 'salidaL1A_3.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1A_2016_4.out', 'salidaL1A_4.out')
+x = 1
+print ">>> Casos L1B"
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1B_2016_1.out', 'salidaL1B_1.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1B_2016_2.out', 'salidaL1B_2.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1B_2016_3.out', 'salidaL1B_3.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1B_2016_4.out', 'salidaL1B_4.out')
+x = 1
+print ">>> Casos L1C"
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1C_2016_1.out', 'salidaL1C_1.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1C_2016_2.out', 'salidaL1C_2.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1C_2016_3.out', 'salidaL1C_3.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1C_2016_4.out', 'salidaL1C_4.out')
+x = 1
+print ">>> Casos L1D"
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1D_2016_1.out', 'salidaL1D_1.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1D_2016_2.out', 'salidaL1D_2.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1D_2016_3.out', 'salidaL1D_3.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1D_2016_4.out', 'salidaL1D_4.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1D_2016_5.out', 'salidaL1D_5.out')
+x = 1
+print ">>> Casos L1E"
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1E_2016_1.out', 'salidaL1E_1.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1E_2016_2.out', 'salidaL1E_2.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1E_2016_3.out', 'salidaL1E_3.out')
+x += 1
+print 'Caso ',x,': ',filecmp.cmp('TestCases/L1E_2016_4.out', 'salidaL1E_4.out')
